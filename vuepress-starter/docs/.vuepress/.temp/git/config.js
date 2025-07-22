53e05f47e01019deb47fa8695e19a75921a2b336
CranberryWong <chen.wang.hec+icachinext@gmail.com>
2025-07-22 17:50:55 +0800

    recommit

--- vuepress-starter/docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/Users/chenwang/Dev/ICACHI/icachinext.github.io/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/Users/chenwang/Dev/ICACHI/icachinext.github.io/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
